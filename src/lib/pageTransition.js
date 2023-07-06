e007f5ce8b79da1b0f6626388d7a13e44154c033
beng <benosmac@gmail.com>
2023-07-06 21:59:14 +1000

    css updates and file restructure

---
+++ src/lib/pageTransition.js
@@ -0,0 +1,5 @@
+// Handle Page in/out transitions
+const bodyEl = document.getElementById('body')
+document.addEventListener('DOMContentLoaded', (event) => {
+    bodyEl?.classList.remove('transition')
+})
