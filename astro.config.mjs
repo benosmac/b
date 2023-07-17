00bf43c0f8f2617e16614e1c01cfb4c080742736
beng <benosmac@gmail.com>
2023-07-17 21:44:27 +1000

    add astro sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // https://astro.build/config
 import { defineConfig } from 'astro/config'
 import notionContentImporter from './src/integrations/notion-content'
-
 import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [mdx(), notionContentImporter()],
+    site: 'https://bengam.in',
+    integrations: [mdx(), notionContentImporter(), sitemap()],
     experimental: {
         assets: true,
     },
