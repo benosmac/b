a2e22a22cb29ff38df9e1516457a6c581a43812c
beng <benosmac@gmail.com>
2023-09-04 23:28:54 +1000

    update to astro 3.0.8

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,18 @@
 // https://astro.build/config
-import { defineConfig } from 'astro/config'
-import notionContentImporter from './src/integrations/notion-content'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config';
+import notionContentImporter from './src/integrations/notion-content';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://bengam.in',
-    integrations: [notionContentImporter(), sitemap(), mdx()],
-    experimental: {
-        assets: true,
-        viewTransitions: true,
-    },
-     scopedStyleStrategy: 'class',
-    markdown: {
-        shikiConfig: {
-            theme: 'dracula-soft',
-        },
-    },
-})
+  site: 'https://bengam.in',
+  integrations: [notionContentImporter(), sitemap(), mdx()],
+  scopedStyleStrategy: 'class',
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula-soft'
+    }
+  }
+});
